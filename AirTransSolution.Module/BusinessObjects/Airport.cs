33329d0ce79b80a39e081971ee6c803fdd7ea6a2
Vadim Zylevich <Vadim.Zylevich@galaktika.by>
2016-12-23 15:42:30 +0300

    first commit

---
+++ AirTransSolution.Module/BusinessObjects/Airport.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using DevExpress.Xpo;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+
+namespace AirTransSolution.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class Airport : BaseObject
+    { 
+        public Airport(Session session)
+            : base(session)
+        {
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { SetPropertyValue("name", ref name, value); }
+        }
+
+//        private IList<Pilot> pilots;
+//        public Pil
+    }
+
+    [DefaultClassOptions]
+    [System.ComponentModel.DefaultProperty("Name")]
+    public class Pilot : BaseObject
+    {
+        public Pilot(Session session) : base(session) { }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetPropertyValue("Name", ref name, value); }
+        }
+
+        private IList<Airplane> airplanes;
+
+        public IList<Airplane> Airplanes
+        {
+            get { return airplanes; }
+            set { SetPropertyValue("Airplanes", ref airplanes, value); }
+        }
+    }
+
+    [DefaultClassOptions]
+    [System.ComponentModel.DefaultProperty("Name")]
+    public class Airplane : BaseObject
+    {
+        public Airplane(Session session) : base(session) { }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetPropertyValue("Name", ref name, value); }
+        }
+    }
+}
---
+++ AirTransSolution.Win/WinApplication.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace AirTransSolution.Win {
+    partial class AirTransSolutionWindowsFormsApplication {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule();
+            this.module3 = new AirTransSolution.Module.AirTransSolutionModule();
+            this.module4 = new AirTransSolution.Module.Win.AirTransSolutionWindowsFormsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // AirTransSolutionWindowsFormsApplication
+            // 
+            this.ApplicationName = "AirTransSolution";
+            this.CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.UseOldTemplates = false;
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.AirTransSolutionWindowsFormsApplication_DatabaseVersionMismatch);
+            this.CustomizeLanguagesList += new System.EventHandler<DevExpress.ExpressApp.CustomizeLanguagesListEventArgs>(this.AirTransSolutionWindowsFormsApplication_CustomizeLanguagesList);
+
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+
+        #endregion
+
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule module2;
+        private AirTransSolution.Module.AirTransSolutionModule module3;
+        private AirTransSolution.Module.Win.AirTransSolutionWindowsFormsModule module4;
+    }
+}
